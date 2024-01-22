a2ea815ba4957a69c1e106bc9a054f6cf67caf62
John Croasdale <38725741+johncroasdale@users.noreply.github.com>
2024-01-22 19:57:37 +0000

    Added CVD and Sec (male) tests to param test pack

--- QRisk3.Engine.Tests/ParamTests.cs
+++ QRisk3.Engine.Tests/ParamTests.cs
@@ -7,8 +7,7 @@ namespace QRisk3.Engine.Tests
     {
         [SetUp]
         public void Setup()
-        {
-            
+        {            
         }
 
         [Test]
@@ -23,16 +22,27 @@ namespace QRisk3.Engine.Tests
                 var actual_engineScores = actual_serviceResult.EngineResults.Where(p => p.EngineName == Core.EPStandardDefinitions.Engines.QRisk3).Single();
                 var actual_QRisk3Score = actual_engineScores.Results.Where(p => p.id.ToString() == Globals.QRiskScoreUri).Single();
                 var actual_QRisk3HeartAgeScore = actual_engineScores.Results.Where(p => p.id.ToString() == Globals.QRiskScoreUri + "HeartAge").SingleOrDefault();
+                var actual_Meta = actual_engineScores.CalculationMeta;
 
                 var expected_serviceResult = test.PredictionModel;
                 var expected_engineScores = expected_serviceResult.EngineResults.Where(p => p.EngineName == Core.EPStandardDefinitions.Engines.QRisk3).Single();
                 var expected_QRisk3Score = expected_engineScores.Results.Where(p => p.id.ToString() == Globals.QRiskScoreUri).Single();
                 var expected_QRisk3HeartAgeScore = expected_engineScores.Results.Where(p => p.id.ToString() == Globals.QRiskScoreUri + "HeartAge").SingleOrDefault();
+                var expected_Meta = expected_engineScores.CalculationMeta;
 
+                // we always get a score, even if it's 0.0
                 Assert.IsTrue(expected_QRisk3Score.score == actual_QRisk3Score.score, test.TestName);
-                Assert.IsTrue(expected_QRisk3HeartAgeScore.score == actual_QRisk3HeartAgeScore.score, test.TestName);
-            }
-                        
+
+                // we don't always get a heart age score (like when CVD = true) so we need to check whether we're expecting one
+                if (expected_QRisk3HeartAgeScore != null)
+                {
+                    Assert.IsTrue(expected_QRisk3HeartAgeScore.score == actual_QRisk3HeartAgeScore.score, test.TestName);
+                }
+
+                // Final assertion is that the calc reasons match
+                Assert.IsTrue(expected_Meta.EngineResultStatus == actual_Meta.EngineResultStatus, test.TestName);
+                Assert.IsTrue(expected_Meta.EngineResultStatusReason == actual_Meta.EngineResultStatusReason, test.TestName);
+            }                        
         }
     }
 }
